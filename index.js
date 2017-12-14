ede5ede10476f3fc36910a1dc17bbbdc327b9bc1
Daniel Dunbar <danielqdunbar@gmail.com>
2017-12-14 03:32:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+=======
+var greetSpecialGuest = "Hello," + specialGuest + "!";
+>>>>>>> 96b93b5b6ce109f0b26df176eba90dcd6c3f5a01
 
 var topic = "space";
 
